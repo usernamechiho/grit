177d07b986510fa552d9e08bb4215f5ed7acd3ff
이치호 <minepanorama@naver.com>
2025-04-17 23:42:58 +0900

    FIX: 프로젝트 세팅, eslint, prettier 추가 및 expo go 추가

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true, // 문자열은 홑따옴표로
+  parser: "typescript", // 타입스크립트 사용
+  semi: true, // 세미콜론 사용
+  useTabs: false, // 탭 사용 금지
+  tabWidth: 2, // 들여쓰기 2칸
+  printWidth: 120, // 줄 바꿈 길이
+  arrowParens: "always", // 화살표 함수 괄호 사용
+};
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,21 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
-import "react-native-reanimated";
-import "../global.css";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import '../global.css';
+import { View } from 'react-native';
+import { Slot } from 'expo-router';
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+// 앱 로딩이 완료되기 전에 스플래시 화면이 자동으로 사라지는 것을 방지
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
+    // 폰트가 로드되면 스플래시 화면 숨기기
     if (loaded) {
       SplashScreen.hideAsync();
     }
@@ -33,12 +26,8 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View className="flex-1">
+      <Slot />
+    </View>
   );
 }
